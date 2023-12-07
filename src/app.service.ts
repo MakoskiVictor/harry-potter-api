7c4bb14bc765c4588a8d12af7bee883c9e5db201
MakoskiVictor <makoski.ed@gmail.com>
2023-12-07 20:27:34 -0300

    add explatantion of routes

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,23 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): object[] {
+    return [
+      {
+        route: '/character',
+        method: 'Get',
+        explanation: 'Get list of characters',
+      },
+      {
+        route: '/character',
+        method: 'Post',
+        explanation: 'Post a new character',
+      },
+      {
+        route: '/character',
+        method: 'Delete',
+        explanation: 'Delete an especific character',
+      },
+    ];
   }
 }
