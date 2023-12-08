369f7963e91ae7b8c06617f55529414b4b97312a
MakoskiVictor <makoski.ed@gmail.com>
2023-12-07 21:42:53 -0300

    add get, post, delete and getById controllers

--- src/character/character.controller.ts
+++ src/character/character.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CharacterService } from './character.service';
+import { CreateCharacterDTO } from 'src/dto/create-character-dto';
 
 @Controller('character')
-export class CharacterController {}
+export class CharacterController {
+  constructor(private characterService: CharacterService) {}
+
+  @Get()
+  getAllCharacters() {
+    return this.characterService.getAllCharacters;
+  }
+
+  @Get(':id')
+  getCharacterById(@Param('id') id: number) {
+    return this.characterService.getCharacterById(id);
+  }
+
+  @Post()
+  postRecipe(@Body() newCharacter: CreateCharacterDTO) {
+    return this.characterService.postCharacter(newCharacter);
+  }
+
+  @Delete(':id')
+  deleteCharacter(@Param('id') id: number) {
+    return this.characterService.deleteCharacter(id);
+  }
+}
